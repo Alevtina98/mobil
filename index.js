eebe8aa127ccb2067b88e124a681dc824f48a8ac
Alevtina <belyakova_as@mail.ru>
2021-09-30 00:11:08 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const HellowWord = () => (
+    <View
+        style={{
+            flex: 1,
+            justifyContent: "center",
+            aligneItems: "center"
+        }}>
+            <Text>Hellow, world! </Text>
+    </View>
+)
+
+export default HellowWord;
+
+    
